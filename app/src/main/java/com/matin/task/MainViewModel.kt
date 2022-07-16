47aeaf761fcbb4edcc708dadb74c1e129af444e9
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 14:04:08 +0430

    viewModel

---
+++ app/src/main/java/com/matin/task/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.matin.task
+
+import androidx.lifecycle.ViewModel
+import com.matin.task.data.network.ApiService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val apiService: ApiService
+): ViewModel() {
+    companion object {
+        const val DEFAULT_PAGE = 1
+        const val DEFAULT_PAGE_SIZE = 30
+    }
+
+
+    suspend fun getFeeds(){
+        apiService.getFeeds(DEFAULT_PAGE, DEFAULT_PAGE_SIZE)
+    }
+}
--- app/src/main/java/com/matin/task/ui/FirstFragment.kt
+++ app/src/main/java/com/matin/task/ui/FirstFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.matin.task.adapter.FeedAdapter
 import com.matin.task.databinding.FragmentFirstBinding
 import com.matin.task.model.Docs
@@ -19,8 +19,7 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+//    val viewModel by viewModels()
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -35,7 +34,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launchWhenStarted {
 
+        }
 
     }
 
