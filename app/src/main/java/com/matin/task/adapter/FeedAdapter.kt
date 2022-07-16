16abe0a7b3e2d78a89dd19cdfba21419f98620b6
Kickass86 <ggbagi@gmail.com>
2022-07-17 00:19:31 +0430

    make item in recyclerview more appropriate

--- app/src/main/java/com/matin/task/adapter/FeedAdapter.kt
+++ app/src/main/java/com/matin/task/adapter/FeedAdapter.kt
@@ -1,10 +1,15 @@
 package com.matin.task.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.matin.task.databinding.FeedItemBinding
 import com.matin.task.model.Docs
+import com.matin.task.model.Media
 
 class FeedAdapter(private val feeds: ArrayList<Docs>): RecyclerView.Adapter<FeedAdapter.FeedHolder>() {
 
@@ -22,12 +27,56 @@ class FeedAdapter(private val feeds: ArrayList<Docs>): RecyclerView.Adapter<Feed
 
     override fun getItemCount() = feeds.size
 
-    inner class FeedHolder(val view: FeedItemBinding): RecyclerView.ViewHolder(view.root) {
+    inner class FeedHolder(private val view: FeedItemBinding): RecyclerView.ViewHolder(view.root) {
         fun bind(feed: Docs) {
             view.apply {
-
+                author.text = feed.author?.name ?: ""
+                answer.text = feed.ownerAnswer
+                voteCount.text = feed.likesCount.toString()
+                commentCount.text = feed.commentCount.toString()
+                manufactureType.text = feed.manuType
+                manufacture.text = feed.manufacturer?.name ?: ""
+                val pictureArrayList = getPictures(feed.media)
+                if (pictureArrayList.size >= 1) {
+                    Glide
+                        .with(this.root.context)
+                        .load(pictureArrayList[0].url)
+                        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                        .into(picture)
+                } else {
+                    picture.visibility = View.GONE
+                }
+                if (feed.manufacturer?.isPartner == true) {
+                    verified.visibility = View.VISIBLE
+                }
+                vote.setOnClickListener { showDialog("vote") }
+                comment.setOnClickListener { showDialog("comment") }
+                share.setOnClickListener { showDialog("share") }
+                more.setOnClickListener { showDialog("more") }
             }
         }
+
+        private fun showDialog(s: String) {
+            val dialogBuilder = AlertDialog.Builder(view.root.context)
+            val message = "You've clicked on \"$s\""
+            dialogBuilder.setTitle("Inform")
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .create()
+                .show()
+        }
+
+        private fun getPictures(media: ArrayList<Media>): ArrayList<Media> {
+            val filterList: ArrayList<Media> = ArrayList()
+            for (medium in media) {
+                if (medium.mediaType.equals("picture")) {
+                    filterList.add(medium)
+                }
+            }
+            return filterList
+        }
     }
+
+
 
 }
